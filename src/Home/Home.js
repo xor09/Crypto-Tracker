1bf156a5c7fbbc4d9261ec10eebe6b1338f152cc
Bishal Suvechha Manindra <70262740+xor09@users.noreply.github.com>
2023-12-29 22:40:56 +0530

    first push

---
+++ src/Home/Home.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+
+
+// Function to simulate real-time updates with random bid and ask prices
+const simulateRealTimeUpdate = (symbol) => ({
+  s: symbol,
+  b: (Math.random() * 100).toFixed(2), // Random bid price
+  a: (Math.random() * 100).toFixed(2), // Random ask price
+});
+
+
+
+function Home() {
+  const [symbolInput, setSymbolInput] = useState("");
+  const [marketWatchList, setMarketWatchList] = useState([]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Simulate real-time updates for each subscribed symbol
+      setMarketWatchList((prevList) =>
+        prevList.map((symbolObj) => simulateRealTimeUpdate(symbolObj.s))
+      );
+    }, 2000); // Update every 2 seconds
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const subscribeToSymbol = () => {
+    const symbol = symbolInput.trim().toUpperCase();
+
+    // Add the subscribed symbol as an object to the market watch list
+    setMarketWatchList((prevList) => [
+      ...prevList,
+      simulateRealTimeUpdate(symbol),
+    ]);
+
+    // Clear input field
+    setSymbolInput("");
+  };
+
+  return (
+    <div>
+      <h1>Market Watch App</h1>
+      <input
+        type="text"
+        value={symbolInput}
+        onChange={(e) => setSymbolInput(e.target.value)}
+        placeholder="Enter symbol"
+      />
+      <button onClick={subscribeToSymbol}>Subscribe</button>
+
+      <div>
+        {marketWatchList.map((data) => (
+          <div
+            key={data.s}
+          >{`${data.s}: Bid - ${data.b}, Ask - ${data.a}`}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
